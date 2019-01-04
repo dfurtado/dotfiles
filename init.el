cdbf5876d0202588e3dd1bb5d0c19022ab66c070
Daniel Furtado <daniel@dfurtado.com>
2019-01-04 15:00:41 +0100

    Added docker related modes + dimmer and icons.

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yaml-mode pylint rust-mode smart-mode-line-atom-one-dark-theme golden-ratio org-bullets elpy dashboard helm-projectile projectile helm magit evil-surround evil diminish highlight-indentation base16-theme use-package))))
+    (docker-compose-mode dockerfile-mode dimmer yaml-mode pylint rust-mode smart-mode-line-atom-one-dark-theme golden-ratio org-bullets elpy dashboard helm-projectile projectile helm magit evil-surround evil diminish highlight-indentation base16-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
